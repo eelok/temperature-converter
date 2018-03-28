31cbba612e741d67e297d77fe2d077da4faa1745
eelok <eelok.ru>
2018-03-28 20:10:15 +0200

    complete fahrenheit part

--- add.js
+++ add.js
@@ -4,17 +4,21 @@ var celciusInput = document.querySelector('#celcius > input');
 var fahrenheitInput = document.querySelector('#fahrenheit > input');
 var kelvinInput = document.querySelector('#kelvin > input');
 
-celciusInput.addEventListener('input', function(){
+function celciusToFahrenheitAndKelvin(){
     var cTemp = parseFloat(celciusInput.value);
     var fTemp = (cTemp * (9/5)) + 32;
     var kTemp = cTemp + 273.15;
     fahrenheitInput.value = fTemp;
     kelvinInput.value = kTemp;
-});
+}
 
-fahrenheitInput.addEventListener('input', function(){
-    var fTemp = fahrenheitInput.value;
-    console.log(fTemp);
-})
+function fahrenheitTocelciusAndKelvin(){
+    var fTemp = parseFloat(fahrenheitInput.value);
+    var cTemp = (fTemp - 32) * (5/9);
+    var kTemp = (fTemp + 459.67) * (5/9);
+    kelvinInput.value = kTemp;
+    celciusInput.value = cTemp;
+}
 
-
+celciusInput.addEventListener('input', celciusToFahrenheitAndKelvin);
+fahrenheitInput.addEventListener('input', fahrenheitTocelciusAndKelvin);
